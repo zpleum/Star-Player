668c659d710b5e0aa2cdf042f983804dacdb734f
zpleum <wiraphat.makwong@gmail.com>
2026-05-04 18:14:07 +0700

    Rename CAP app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'me.zpleum',
-  appName: 'star-player',
+  appName: 'Star Player',
   webDir: 'out'
 };
 
